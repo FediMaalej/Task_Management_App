3a5b76fea585ff0fd57d97138da49197e4e6a984
FediMaalej <usertwo@xtendplex.com>
2024-01-13 21:11:20 +0100

    local setup completed

--- Task_Management_Back/app.js
+++ Task_Management_Back/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
